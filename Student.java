18a38ae92cd67ea13f4a6c25ab158ac46bcce6bd
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-04-29 19:35:34 -0300

    save

---
+++ Student.java
@@ -0,0 +1,30 @@
+package Student1;
+
+public class Student {
+    String name;
+    int id;
+    String course;
+    
+    
+
+    // Constructor
+    public Student(String name, int id, String course) {
+        this.name = name;
+        this.id = id;
+        this.course = course;
+        
+    }
+
+    // Method to display student details
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Course: " + course);
+        
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Tanuja", 101, "Computer Science");
+        student1.displayDetails();
+    }
+}
