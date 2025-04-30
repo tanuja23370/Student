03d4e0a458cd983bba912c48d1afad17d64b995f
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-04-29 21:08:46 -0300

    save

--- Student.java
+++ Student.java
@@ -4,14 +4,16 @@ public class Student {
     String name;
     int id;
     String course;
+    long phoneNumber;
     
     
 
     // Constructor
-    public Student(String name, int id, String course) {
+    public Student(String name, int id, String course, long phoneNumber) {
         this.name = name;
         this.id = id;
         this.course = course;
+        this.phoneNumber = phoneNumber;
         
     }
 
@@ -20,11 +22,12 @@ public class Student {
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
         System.out.println("Course: " + course);
+        System.out.println("Phone Number: " + phoneNumber);
         
     }
 
     public static void main(String[] args) {
-        Student student1 = new Student("Tanuja", 101, "Computer Science");
+        Student student1 = new Student("Tanuja", 101, "Computer Science", 1234567890L);
         student1.displayDetails();
     }
 }
